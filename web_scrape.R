5ca2ffc61302dba0f8af6afd8d88f0ff0c044bfb
rzj1019 <johnson.ryan1019@gmail.com>
2022-02-04 20:52:18 -0700

    new lapply to extract and check data

--- web_scrape.R
+++ web_scrape.R
@@ -9,15 +9,16 @@ mapanddata <- str_locate(footer, "Map and Data")
 
 
 #### AWS API:eviction-lab-data-downloads #####
-
 bucketname <- "eviction-lab-data-downloads"
 bucketdata <- aws.s3::get_bucket(bucketname)
 webpath <- paste0("s3://",bucketname, "/")
-keynames<- data.table(keys = as.character())
+keynames<- c()
 
 # loop to grab key list
 for (i in 1:length(bucketdata)) {
    key <- c(bucketdata[[i]][[1]])
-   keynames <- rbind(keynames, key, use.names = FALSE)
+   keynames <- c(keynames,key)
 }
 
+# Run data pull and check
+lapply(keynames, loadncheck.bucket)
