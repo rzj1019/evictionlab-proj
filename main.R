11823af225d93ab658e1b2c276aa516549a561f3
rzj1019 <johnson.ryan1019@gmail.com>
2022-02-06 21:15:57 -0700

    added lappy to generate graphs

--- main.R
+++ main.R
@@ -1,6 +1,24 @@
+############################################
+# Main script to source files and define global variables
+############################################
+rm(list = ls())
+graphics.off()
 
-
-
+# Load packages and set directories
 source("system_init.R")
+
+# Load users functions
 source("functions.R")
-source("web_scrape.R")
+
+# Pull and check data for updates
+# run as needed.
+##  source("web_scrape.R")
+
+# Load in E-Lab data from dir.
+source("data_explore.R")
+
+
+#### Plots ####
+
+# generate eviction rates by state: year & county
+lapply(states.allcounties, evict.rate.state.county)
