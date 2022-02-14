4fe7b88b2bc875b06c89bd8b265057345305a037
rzj1019 <johnson.ryan1019@gmail.com>
2022-02-14 16:34:01 -0500

    added pov. rate vs  med prop val function

--- data_explore.R
+++ data_explore.R
@@ -31,12 +31,12 @@ dem.names <- names(all.counties)[c(5,13:20)]
 metrics <- names(all.counties)[c(6:12,21:24)]
 
 
-#### Function to plot each counties eviction rate by state
+#### Function to plot each counties eviction rate by state ####
 evict.rate.state.county <- function(state) {
 
    state.data <- all.counties[parent.location == state]
    state.years <- state.data[ , unique(year)] %>% sort()
-   pdf.path <- paste0(plots.dir, "/", state, "_evict_rate_by_year.pdf")
+   pdf.path <- paste0(plots.dir, "/evict_rate/", state, "_by_year.pdf")
    state.name <- state.data[ , parent.location] %>% unique()
 
    # open pdf
@@ -46,8 +46,10 @@ evict.rate.state.county <- function(state) {
    # Run state through plot function
    for (y in seq_along(state.years)) {
       curr.year <- state.years[y]
-      temp.plot <- ggplot2::ggplot(state.data[year == curr.year],
+      plot.data <- state.data[year == curr.year]
+      temp.plot <- ggplot2::ggplot(plot.data,
                                    aes(x = name, y = eviction.rate)) +
+         geom_hline(yintercept = plot.data[!is.na(eviction.rate) , mean(eviction.rate)]) +
          geom_bar(stat = "identity", width = 0.5) +
          labs(title = (paste0(state.name, " Eviction Rates by County")),
               subtitle = curr.year) +
@@ -61,4 +63,44 @@ evict.rate.state.county <- function(state) {
    dev.off()
    message("Done plotting, ", state.name, ", PDF ready for viewing.")
    message("-----------------------------------")
+}
+
+
+#### Function plot all combinations of correlation from list; each state ####
+poverty.median.val <- function(state) {
+
+   state.data <- all.counties[parent.location == state]
+   state.name <- state.data[ , parent.location] %>% unique()
+   n.counties <- state.data[ , name] %>% unique()
+   n.years <- state.data[ , unique(year)] %>% sort()
+   pdf.path <- paste0(plots.dir, "/corr/", state, "_by_year.pdf")
+
+   # open pdf
+   pdf(file = pdf.path, width = 11, height = 8.5)
+   message("Plotting data for ", state.name)
+
+
+   # Plot all counties: provery rate vs. med. prop. val.
+   for (s in seq_along(n.counties)) {
+
+      plot.data <- state.data %>% setorder("name")
+      county <- n.counties[s]
+      temp.plot <- ggplot2::ggplot(plot.data[name == county],
+                                   aes(x = year,
+                                       y = median.property.value)) +
+         geom_point() +
+         geom_text_repel(aes(label = poverty.rate)) +
+         labs(title = paste0(state.name, ": ", county),
+              subtitle = "Median Property Value by Year") +
+         scale_x_continuous("Years", labels = n.years, breaks = n.years)
+
+
+      #plot
+      plot(temp.plot)
+      message("Plotted: ", state.name, " - ", county)
+
+   }
+
+   dev.off()
+   message("---------------------")
 }
