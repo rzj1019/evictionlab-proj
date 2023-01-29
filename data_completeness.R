dfbd051383d6f4ba69affad952400767f05e5c35
rzj1019 <johnson.ryan1019@gmail.com>
2023-01-29 15:40:02 -0500

    added chromebook variation

--- data_completeness.R
+++ data_completeness.R
@@ -6,16 +6,22 @@
 
 # Generate tables of metadata of each state according to block.groups, tracts, and counties----------------------------------------------------------
 census.levels <- c("block.groups", "tracts", "counties")
-block.groups.metadata <- eviction_metadata(census.levels[1])
-tracts.metadata <- eviction_metadata(census.levels[2])
-counties.metadata <- eviction_metadata(census.levels[3])
+for (t in census.levels %>% seq_along()) {
+   name <- census.levels[t]
+   tbl <- try(get_metadata(name))
+   if()
+   assign(paste0(name,".metadata"), tbl)
+}
 metadata.all <- rbind(block.groups.metadata,
                       tracts.metadata,
                       counties.metadata)
 
 # Write to Google -------------------------------------------------------------------------------------------------------------------------------
-TDA.file <- as_sheets_id("https://docs.google.com/spreadsheets/d/1o1YUPIleg0LeakjJ9AePPJlx2c6nA_mEXkQxLv5nXsg/edit#gid=0")
-googlesheets4::write_sheet(metadata.all,
-                           ss = TDA.file,
-                           sheet = "Metadata")
+if(metadata.all %>% nrow() == 132){
+   TDA.file <- as_sheets_id("https://docs.google.com/spreadsheets/d/1o1YUPIleg0LeakjJ9AePPJlx2c6nA_mEXkQxLv5nXsg/edit#gid=0")
+   googlesheets4::write_sheet(metadata.all,
+                              ss = TDA.file,
+                              sheet = "Metadata")
+}
+
 
